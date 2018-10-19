cdf6670cf7d43d68230481a12bccfaef27678174
Nishchal Gaba <nishgaba.ai@gmail.com>
2018-10-19 13:14:15 +0530

    First Commit - Test Repositories

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # unrealai
 Image/Machine Learning Tool Interface
+
+
+# Installation
+Run the following to install:
+
+'''python
+pip install unrealai
+'''
+
+## Usage
+'''python
+from helloworld import say_hello
+
+# Generate "Hello, World!"
+say_hello()
+
+# Generate "Hello, Everybody!"
+say_hello("Everybody")
+'''
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+
+setup(
+	name='unrealai',
+	version='0.0.1',
+	description='Say hello!',
+	py_modules=["helloworld"],
+	package_dir={'':'src'},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"License :: OSI Approved :: GNU General Public License v3",
+		"Operating System :: OS Independent",
+		],
+		long_description=long_description,
+		long_description_content_type="text/markdown",
+		url= "https://github.com/nishgaba-ai/unrealai",
+		author="Nishchal Gaba",
+		author_email="nishgaba.ai@gmail.com",
+)
