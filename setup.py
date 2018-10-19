abdbce1b0920033f07182a7928103b62d2237489
Nishchal Gaba <nishgaba.ai@gmail.com>
2018-10-19 14:03:29 +0530

    Switched to Version 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name='unrealai',
-	version='0.0.1',
+	version='0.0.2',
 	description='Say hello!',
 	py_modules=["helloworld"],
 	package_dir={'':'src'},
