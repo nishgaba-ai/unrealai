fa0ba43b8897b2fb394d0b2f478ef13f51a45142
Nishchal Gaba <nishgaba.ai@gmail.com>
2018-10-19 13:18:38 +0530

    Updated License in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 		"Programming Language :: Python :: 3",
 		"Programming Language :: Python :: 3.6",
 		"Programming Language :: Python :: 3.7",
-		"License :: OSI Approved :: GNU General Public License v3",
+		"License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
 		"Operating System :: OS Independent",
 		],
 		long_description=long_description,
